ccfda779dca45a5ae17b13a2be5898731232883f
mmart@inbox.ru <mmart@inbox.ru>
2022-07-01 08:06:18 +0300

    Fix: Числа как числа в excel, а не строки

--- templater/Classes/XlsxFiller.cs
+++ templater/Classes/XlsxFiller.cs
@@ -1,4 +1,5 @@
 ﻿using Aspose.Cells;
+using System.Text.Json;
 using templater.contracts;
 
 namespace templater.Classes;
@@ -59,27 +60,33 @@ public class XlsxFiller
     void FillCell(Cell cell, IDictionary<string, object> replacements, bool ignoreZeroes)
     {
         var cellValue = cell.Value?.ToString() ?? string.Empty;
-        if (string.IsNullOrWhiteSpace(cellValue) || !cellValue.Contains("{{") || !cellValue.Contains("}}"))
+        if (string.IsNullOrWhiteSpace(cellValue) || cellValue.Length < 5 || !cellValue.Contains("{{") || !cellValue.Contains("}}"))
             return;
 
-        // ищем какие там паттерны
         var repls = replacements.AsEnumerable().Union(_defaultReplacements.Replacements);
         foreach (var repl in repls)
         {
             var template = "{{" + repl.Key + "}}";
+            // если полное совпадение с шаблоном
+            if (cellValue.Equals(template, StringComparison.OrdinalIgnoreCase))
+            {
+                ReplaceCellValue(cell, repl.Value ?? string.Empty, ignoreZeroes);
+                return;
+            }
+
+            // вообще нет такого шаблона
             if (!cellValue.Contains(template, StringComparison.OrdinalIgnoreCase))
                 continue;
 
             // на что менять
             var replaceTo = repl.Value?.ToString() ?? string.Empty;
             // игнорирование нулей
-            if (ignoreZeroes && decimal.TryParse(replaceTo, out decimal zeroValue) && zeroValue == 0)
+            if (ignoreZeroes && decimal.TryParse(replaceTo, out var zeroValue) && zeroValue == 0)
                 replaceTo = string.Empty;
 
             // замена
             cellValue = cellValue.Replace(template, replaceTo, StringComparison.OrdinalIgnoreCase);
         }
-        // запись нового значения
         cell.PutValue(cellValue);
     }
 
@@ -88,6 +95,7 @@ public class XlsxFiller
     /// </summary>
     void FillXlsHeaders(Cells cells, Dictionary<string, object> replacements)
     {
+#warning вот тут наверное надо собрать новый словарь в lowerCase, тогда FullCell сможет без перебора обходится
         for (var r = cells.MinDataRow; r <= cells.MaxDataRow; r++)
             for (var c = cells.MinDataColumn; c <= cells.MaxDataColumn; c++)
                 FillCell(cells[r, c], replacements, ignoreZeroes: false);
@@ -98,6 +106,7 @@ public class XlsxFiller
     /// </summary>
     void FillXlsRow(int rowIndex, Cells cells, IDictionary<string, object> rowData, bool ignoreZeroValues)
     {
+#warning вот тут наверное надо собрать новый словарь в lowerCase, тогда FullCell сможет без перебора обходится
         for (var c = cells.MinDataColumn; c <= cells.MaxDataColumn; c++)
             FillCell(cells[rowIndex, c], rowData, ignoreZeroValues);
     }
@@ -142,5 +151,22 @@ public class XlsxFiller
                     return r;
             }
         return -1;
+    }
+
+    static void ReplaceCellValue(Cell cell, object value, bool ignoreZeroes)
+    {
+        if (value is not JsonElement || ((JsonElement)value).ValueKind != JsonValueKind.Number)
+        {
+            cell.PutValue(value.ToString());
+            return;
+        }
+        // если это число
+        var je = (JsonElement)value;
+        if (je.TryGetInt64(out var i))
+            cell.PutValue(ignoreZeroes && i == 0 ? string.Empty : i);
+        else if (je.TryGetDouble(out var d))
+            cell.PutValue(ignoreZeroes && d == 0 ? string.Empty : d);
+        else
+            cell.PutValue(string.Empty);
     }
 }
